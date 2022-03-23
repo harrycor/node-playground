e576095b19601dd490337072ce36fa8e671055ad
Harry Corradi <harrycorradi@protonmail.com>
2022-03-23 19:51:27 -0400

    first commit for playing around with nope and npm

---
+++ index.js
@@ -0,0 +1,12 @@
+let p = require('path');
+
+console.log(p.join(__dirname, "testing.js"));
+
+
+// let greeting = "Hello World";
+
+// for(let i=0; i< 100; i++) {
+//     console.log(i);
+// }
+
+// console.log(greeting);
---
+++ server/server.js
@@ -0,0 +1,26 @@
+let p = require("path");
+let fs = require("fs");
+let request = require("request");
+
+let filePath = p.join(__dirname, "../data.json");
+
+request("http://reddit.com/r/popular/.json", (err, res, body) => {
+    if (err) console.log(err);
+
+    JSON.parse(body).data.children.forEach(item => {
+        // console.log(item.data.title)
+        fs.appendFileSync(filePath, item.data.title + "\n");
+    })
+    // fs.writeFile(filePath, res.body, err => {
+    //     if (err) console.log(err);
+    // });
+})
+
+// fs.readFile(filePath, {
+//     encoding: "UTF-8"
+// }, (err, data) => {
+//     let dataParsed = JSON.parse(data);
+
+//     console.log(dataParsed.name);
+//     console.log(dataParsed.shirtColor);
+// })
